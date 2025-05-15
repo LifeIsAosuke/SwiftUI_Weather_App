8bbba558dc820f900762753efcf1fec5da8e2e37
Aoi-Suzuki <aoidon.lifeistech@gmail.com>
2025-05-16 06:06:36 +0900

    Initial Commit

---
+++ Weather/WeatherApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeatherApp.swift
+//  Weather
+//
+//  Created by A S on 2025/05/16.
+//
+
+import SwiftUI
+
+@main
+struct WeatherApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ WeatherTests/WeatherTests.swift
@@ -0,0 +1,17 @@
+//
+//  WeatherTests.swift
+//  WeatherTests
+//
+//  Created by A S on 2025/05/16.
+//
+
+import Testing
+@testable import Weather
+
+struct WeatherTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
